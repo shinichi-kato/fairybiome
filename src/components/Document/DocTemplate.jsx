54eeeab01dad421ec2d5835e78d974bb826b02cd
shinichi-kato <skato21r@iris.eonet.ne.jp>
2020-06-19 00:22:25 +0900

    template theme injection

--- src/components/Document/DocTemplate.jsx
+++ src/components/Document/DocTemplate.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,7 +9,60 @@ import Box from '@material-ui/core/Box';
 
 import FairyBiomeIcon from '../../icons/FairyBiome';
 
+const useStyles = makeStyles((theme) => ({
+  article: {
+    color:theme.palette.text.primary,
+    fontSize:theme.typography.fontSize,
+    '& p':{
+      fontFamily:theme.typography.body1.fontFamily,
+      fontSize:theme.typography.body1.fontSize,
+      fontWeight:theme.typography.body1.fontWeight,
+      lineHeight:theme.typography.body1.lineHeight,
+      letterSpacing:theme.typography.body1.letterSpacing,     
+    },
+    '& h1':{
+      fontFamily:theme.typography.h1.fontFamily,
+      fontSize:theme.typography.h1.fontSize,
+      fontWeight:theme.typography.h1.fontWeight,
+      lineHeight:theme.typography.h1.lineHeight,
+      letterSpacing:theme.typography.h1.letterSpacing,
+    },
+    '& h2':{
+      fontFamily:theme.typography.h2.fontFamily,
+      fontSize:theme.typography.h2.fontSize,
+      fontWeight:theme.typography.h2.fontWeight,
+      lineHeight:theme.typography.h2.lineHeight,
+      letterSpacing:theme.typography.h2.letterSpacing,
+    },
+    '& h3':{
+      fontFamily:theme.typography.h3.fontFamily,
+      fontSize:theme.typography.h3.fontSize,
+      fontWeight:theme.typography.h3.fontWeight,
+      lineHeight:theme.typography.h3.lineHeight,
+      letterSpacing:theme.typography.h3.letterSpacing,
+    },
+    '& h4':{
+      fontFamily:theme.typography.h4.fontFamily,
+      fontSize:theme.typography.h4.fontSize,
+      fontWeight:theme.typography.h4.fontWeight,
+      lineHeight:theme.typography.h4.lineHeight,
+      letterSpacing:theme.typography.h4.letterSpacing,
+    },
+    '& h5':{
+      fontFamily:theme.typography.h5.fontFamily,
+      fontSize:theme.typography.h5.fontSize,
+      fontWeight:theme.typography.h5.fontWeight,
+      lineHeight:theme.typography.h5.lineHeight,
+      letterSpacing:theme.typography.h5.letterSpacing,
+    },
+
+  },
+  
+}));
+
+
 const DocumentTemplate = ({ data, pageContext, location }) => {
+  const classes = useStyles();
   const article = data.markdownRemark;
   const siteTitle = data.site.siteMetadata.title;
   const { previous, next } = pageContext;
@@ -30,7 +84,9 @@ const DocumentTemplate = ({ data, pageContext, location }) => {
         data:{article.frontmatter.date}
       </Box>
       <Box>
-        <div dangerouslySetInnerHTML={{ __html: article.html }} />
+        <div 
+          className={classes.article}
+          dangerouslySetInnerHTML={{ __html: article.html }} />
       </Box>
     </Box>
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -7,6 +7,7 @@ export default function Component() {
       <center>
         <Typography
           color="primary"
+          
           component="span"
           gutterBottom
           paragraph
